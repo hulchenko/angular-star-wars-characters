b15519b4c0ece04c6f6783b84d5c349f971ffe55
Vadym Hulchenko <vhulchenko@flyht.com>
2022-05-26 21:08:56 -0600

    remove dashboard and user details components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'star-wars-characters';
-  rootItems = ['Apples', 'Bananas', 'Cherries'];
-
-  addNewItems(newItem) {
-    this.rootItems.push(newItem)
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    UserDetailComponent,
-    CartComponent
   ],
   imports: [
     BrowserModule,
