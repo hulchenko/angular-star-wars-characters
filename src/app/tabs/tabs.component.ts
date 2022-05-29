e21f27208303ef83ba77d80e3d0dbc14399bcf67
Vadym Hulchenko <vhulchenko@flyht.com>
2022-05-29 14:41:28 -0600

    use event emitter to pass values

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
+  characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Darth Vader', side: '' },
+  ]
+  chosenList = 'all';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
+  onSideChosen(passedCharacterInfo) {
+    const pos = this.characters.findIndex((char) => {
+      return char.name === passedCharacterInfo.name;
+    })
+    this.characters[pos].side = passedCharacterInfo.side;
+  }
+
+  getCharacters() {
+    if (this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+
+    return this.characters.filter((i) => {
+      return i.side === this.chosenList;
+    })
   }
 
 }
